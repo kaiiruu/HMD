d57c1742c13e4d21b87a1e5f8f8dde04ea5fa968
kaiiruu <120348434+kaiiruu@users.noreply.github.com>
2026-05-10 21:09:32 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const title = document.querySelector('.title')
+const text = 'Happy Mother\'s Day po!' .split('') //insert text here
+for (let index = 0; index < text.length; index++) {
+    if (text[index] !== ' ') {
+        title.innerHTML += `<span>${text[index]}<span/>`
+    } else {
+        title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+    const randomDelay = Math.random() * 2;
+    element.style.animationDelay = `${randomDelay}s`;
+});
+
